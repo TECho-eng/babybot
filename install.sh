28b46dfe3f4a05400d883d19b9e1c1c7cbb959a8
TECho-eng <78675047+TECho-eng@users.noreply.github.com>
2021-02-10 11:13:46 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,4 +15,4 @@ npm i got
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
 
-echo "[*] SUKSES BRO SILAHKAN KETIK \"node index.js\" JANGAN LUPA SUBREK CHANNEL GW"
+echo "[*] SUCCESS BRO PLEASE TYPE \"node index.js\" wa.me/917356145338"
